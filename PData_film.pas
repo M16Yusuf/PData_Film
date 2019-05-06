a7b655d9f0c5456fa6b1d4b5aefbe3a11b389b6f
Muhammad Yusuf <yusufsmd58@gmail.com>
2019-05-06 12:35:07 +0700

    menulis record

--- PData_film.pas
+++ PData_film.pas
@@ -1,4 +1,18 @@
 program Pdata_film;
+uses crt;
+
+type 
+  PData = ^TData; 
+  TData = record
+    Judul : String[15];
+    Sutradara : String[10];
+    genre : string[7];
+    Tahun : integer[4];
+    rating : real;
+    info : integer;
+    prev,next:PData;
+  end;
+
 begin
   writeln('hello wolrd');
   writeln('Coba 123');
