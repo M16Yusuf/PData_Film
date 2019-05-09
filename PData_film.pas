20bb0198d8d6cbce8b500f0e89c5fcaa25ecf3b4
alif ramdhan kusaeri <kusaerialif04@gmail.com>
2019-05-09 11:10:27 +0700

    kk

--- PData_film.pas
+++ PData_film.pas
@@ -4,13 +4,13 @@ uses crt;
 type 
   PData = ^TData; 
   TData = record
-    Judul : String[15];
-    Sutradara : String[10];
-    genre : string[7];
-    Tahun : integer[4];
-    rating : real;
-    info : integer;
-    prev,next:PData;
+       Judul : String[15];
+       Sutradara : String[10];
+       genre : string[7];
+       Tahun : integer[4];
+       rating : real;
+       info : integer;
+       prev,next:PData;
   end;
 
 procedure penciptaan(var awal,akhir:PData);
@@ -22,4 +22,22 @@ procedure penciptaan(var awal,akhir:PData);
 begin
   writeln('hello wolrd');
   writeln('Coba 123');
-end.ngentot
+end.
+
+
+function menu :integer;
+  var pil:integer;
+begin
+    writeln('Aplikasi Data filem');
+    writeln('Disusun Oleh : ');
+    writeln('  - Alif Ramdhan Kusaeri (10118365)');
+    writeln('  - Muhammad Yusup       (10118370)');
+    writeln('  - Gifar Fadilah        (10018   )');
+    writeln('-----------------------------------');
+    writeln();
+    writeln('1. Tambah Filem');
+    writeln('2. Edit Filem');
+    writeln('3. Hapus Filem');
+    writeln('4. Urutkan Filem');
+    writeln('') 
+end;
