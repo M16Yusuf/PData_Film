693065de5a8b4b9952ab82506894c1a8de023320
Muhammad Yusuf <yusufsmd58@gmail.com>
2019-05-14 03:54:16 +0700

    beres aah aink maah

--- PData_film.pas
+++ PData_film.pas
@@ -22,6 +22,7 @@ var
   banyakdata : integer;
   pilihan_menu : integer;
   awal,akhir : PData;
+  data : Tinfo;
 
 procedure penciptaan(var awal,akhir:PData);
   begin
@@ -66,10 +67,10 @@ begin
       writeln('---------------------------------------------------------------------');
       writeln('|      Judul Film       |  Sutradara  | Genre | Tahun | Rating Film |');
       writeln('---------------------------------------------------------------------');
-      tamp := akhir;
+      tamp := awal;
       tamp := tamp^.next;
       banyakdata:=4;
-      while tamp <> nil do
+      while tamp <> awal do
         gotoxy(1, banyakdata);write('|');
         gotoxy(2, banyakdata);write(tamp^.info.judul);
         gotoxy(25, banyakdata);write('|');
@@ -79,11 +80,11 @@ begin
         gotoxy(47, banyakdata);write('|');
         gotoxy(48, banyakdata);write(tamp^.info.tahun);
         gotoxy(55, banyakdata);write('|');
-        gotoxy(56, banyakdata);write(tamp^.info. rating); 
+        gotoxy(56, banyakdata);write(tamp^.info. rating:2:0); 
         gotoxy(69, banyakdata);write('|');
         banyakdata:=banyakdata + 1;
       end;       
-      write('tekan enter untuk melanjutkan . . .'); readln();
+      readln();
   end;
 
 procedure sisipdepan (var awal, akhir : Pdata; data : Tinfo);
@@ -136,7 +137,7 @@ begin
 		end;
 end;
 
-procedure tambahdata (var awal, akhir : PData; data : Tinfo);
+procedure tambahdata(var awal, akhir : PData; data : Tinfo);
 var
    bantu:PData;
 begin
@@ -165,7 +166,7 @@ begin
      end;
 end;
 
-procedure menutambahdatabelakang(data:Tinfo);
+procedure menutambahdatabelakang;
 begin
 	clrscr;
 	writeln('Masukan Data');
@@ -178,7 +179,7 @@ begin
 	sisipbelakang(awal, akhir, data);
 end;
 
-procedure menutambahfilm(data:Tinfo);
+procedure menutambahfilm;
 var
 	x, y, u : integer;
 begin
@@ -236,7 +237,7 @@ procedure hapustengah(var awal,akhir:PData);//salman
 var
    phapus:PData;
    posisihapus:string;
-   x,y,u:integer;
+
 begin
      if awal=nil then
         writeln('Data Kosong')
@@ -266,31 +267,200 @@ begin
                readln;
           end;
           clrscr;
+      end;
 end;
 
-procedure menuhapusfilm; //Salman
+procedure hapusfilm;
 var
-pilih:char;
-x,y,u:integer;
+  pilih:char;
+  x,y:integer;
 begin
      x:=5;
      y:=1;
-     gotoxy(x,y+2);writeln('  Menu Hapus  ');
+     gotoxy(x,y+1);writeln('  Menu Hapus  ');
      gotoxy(x,y+2);writeln('--------------');
-     gotoxy(x,y+2);writeln('1. Hapus Depan');
-     gotoxy(x,y+2);writeln('2. Hapus Belakang');
-     gotoxy(x,y+2);writeln('3. Hapus Tengah');
+     gotoxy(x,y+3);writeln('1. Hapus Depan');
+     gotoxy(x,y+4);writeln('2. Hapus Belakang');
+     gotoxy(x,y+5);writeln('3. Hapus Tengah');
+     gotoxy(x,y+6);write('Pilhan anda :  ');readln(pilih);
      case pilih of
      '1':begin
               hapusdepan(awal,akhir);
          end;
      '2':begin
-              hapusbelakang(awal;akhir);
+              hapusakhir(awal,akhir);
          end;
      '3':begin
               hapustengah(awal,akhir);
           end;
      end;
+end;
+
+procedure edit (awal : Pdata; data : Tinfo);
+var
+	bantu : Pdata;
+	cari : string;
+	a, b : integer;
+begin
+	clrscr;
+	a := 5;
+	b := 1;
+	bantu := awal;
+	gotoxy(a,b+1);write('Judul yang dicari   : ');readln(cari);
+	while (bantu^.info.judul <> cari) and (bantu <> akhir) do
+		bantu := bantu^.next;
+	if bantu^.info.judul = cari then
+	begin
+	   	gotoxy(a,b+2);writeln('Judul      : ',data.judul);
+     	gotoxy(a,b+3);writeln('Sutradara  : ',data.sutradara);
+     	gotoxy(a,b+4);writeln('genre      : ',data.genre);
+     	gotoxy(a,b+5);writeln('tahun      : ',data.tahun);
+      gotoxy(a,b+6);writeln('rating     : ',data.rating);
+
+     	gotoxy(a,b+7);writeln('Data dirubah menjadi :');
+     	gotoxy(a,b+8);write('Judul       : ');
+     	gotoxy(a,b+9);write('Sutradara   : ');
+     	gotoxy(a,b+10);write('Genre      : ');
+     	gotoxy(a,b+11);write('tahun      : ');
+      gotoxy(a,b+12);write('rating     : ');
+
+			gotoxy(a+16,b+8);readln(bantu^.info.judul);
+			gotoxy(a+16,b+9);readln(bantu^.info.sutradara);
+			gotoxy(a+16,b+10);readln(bantu^.info.genre);
+			gotoxy(a+16,b+11);readln(bantu^.info.tahun);
+      gotoxy(a+16,b+12);readln(bantu^.info.rating);
+
+     	data.judul := bantu^.info.judul;
+     	data.sutradara := bantu^.info.sutradara;
+     	data.genre := bantu^.info.genre;
+     	data.tahun := bantu^.info.tahun;
+      data.rating := bantu^.info.rating;
+
+     	gotoxy(a,b+14);writeln('Data berhasil dirubah');readln;
+			clrscr;
+	end
+	else
+		writeln('Data tidak ditemukan');
+		clrscr;
+end;
+procedure pengurutanjudul (var awal, akhir : Pdata);
+var
+	p1,p2,pmin : Pdata;
+	temp : Tinfo;
+	x, y : integer;
+begin
+	clrscr;
+	x := 5;
+	y := 1;
+	if awal = nil then
+		writeln('Data Kosong')
+	else
+	begin
+		p1 := awal;
+		while p1 <> akhir do
+		begin
+			p2 := p1^.next;
+			pmin := p1;
+			while p2 <> awal do
+			begin
+				if p2^.info.judul < pmin^.info.judul then
+					pmin := p2;
+				p2 := p2^.next;
+			end;
+			temp := pmin^.info;
+			pmin^.info := p1^.info;
+
+			p1^.info := temp;
+			p1 := p1^.next;
+		end;
+	end;
+	gotoxy(x,y+8);writeln('Pengurutan judul berhasil. Tekan enter untuk melanjutkan !');readln;
+	clrscr;
+end;
+procedure pengurutanjuduldesc (var awal, akhir : Pdata);
+var
+	p1,p2,pmin : Pdata;
+	temp : Tinfo;
+	x, y : integer;
+begin
+	x := 5;
+	y := 1;
+	clrscr;
+	if awal = nil then
+		writeln('Data Kosong')
+	else
+	begin
+		p1 := awal;
+		while p1 <> akhir do
+		begin
+			p2 := p1^.next;
+			pmin := p1;
+			while p2 <> awal do
+			begin
+				if p2^.info.judul > pmin^.info.judul then
+					pmin := p2;
+				p2 := p2^.next;
+			end;
+			temp := pmin^.info;
+			pmin^.info := p1^.info;
+			p1^.info := temp;
+			p1 := p1^.next;
+		end;
+	end;
+	gotoxy(x,y+8);writeln('Pengurutan judul berhasil. Tekan enter untuk melanjutkan !');readln;
+	clrscr;
+end;
+
+procedure menu_pengurutan;
+var
+	x, y : integer;
+  pilih:integer;
+begin
+		 x := 5;
+		 y := 1;
+     clrscr;
+     gotoxy(x,y+1);writeln('Pengurutan Judul film');
+     gotoxy(x,y+2);writeln('---------------------');
+     gotoxy(x,y+3);writeln('1. Judul (Ascending) ');
+     gotoxy(x,y+4);writeln('2. Judul (Descending)'); 
+     gotoxy(x,y+6);write('Pilihan  : ');readln(pilih);
+     case pilih of
+     1 : begin
+           pengurutanjudul(awal,akhir);
+           end;
+     2 : begin
+           pengurutanjuduldesc(awal,akhir);
+           end;
+      end;
+end;
+
+procedure pencarianjudul (awal : Pdata);
+var
+	bantu : Pdata;
+	cari : string;
+	x, y :integer;
+begin
+	x := 5;
+	y := 1;
+	clrscr;
+	gotoxy(x,y+1);write('Judul yang dicari   : ');readln(cari);
+	bantu := awal;
+	while (bantu^.info.judul <> cari) and (bantu <> akhir) do
+		bantu := bantu^.next;
+	if bantu^.info.judul = cari then
+	begin
+		gotoxy(x,y+3);writeln('Judul 		  : ',bantu^.info.judul);
+		gotoxy(x,y+4);writeln('Sutradara 	: ',bantu^.info.sutradara);
+		gotoxy(x,y+5);writeln('Genre 		  : ',bantu^.info.genre);
+		gotoxy(x,y+6);writeln('Tahun  	  : ',bantu^.info.tahun);
+    gotoxy(x,y+7);writeln('Rating     : ',bantu^.info.rating);
+	end
+	else
+		begin
+			writeln('Judul tidak ditemukan');
+			readln;
+		end;
+		readln;
 end;
 
 
@@ -306,12 +476,12 @@ begin
     writeln('  - Muhammad Salman.A.F  (10018389)');
     writeln('-----------------------------------');
     writeln();
-    writeln('1. Tambah Filem');
-    writeln('2. Edit Filem');
-    writeln('3. Hapus Filem');
-    writeln('4. Urutkan Filem');
-    writeln('5. Cari Filem');
-    writeln('6. Tampil Data Filem dari Load');
+    writeln('1. Tambah Film');
+    writeln('2. Edit Film');
+    writeln('3. Hapus Film');
+    writeln('4. Urutkan Film');
+    writeln('5. Cari Film');
+    writeln('6. Tampil Data Film');
     writeln('-----------------------------------');
     write('Pilhan anda :  '); readln(pil); 
     menu := pil;
@@ -324,15 +494,15 @@ begin
         clrscr;
         pilihan_menu := menu();
         case pilihan_menu of 
-          1 : menutambahfilm(Tinfo);
-          2 : edit_filem;
-          3 : menuhapusfilm;
-          4 : urut_filem;
-          5 : cari_filem;
+          1 : menutambahfilm;
+          2 : edit(awal,data);
+          3 : hapusfilm;
+          4 : menu_pengurutan;
+          5 : pencarianjudul(awal);
           6 : tampilDataFilm(awal);
           0 :;
           else writeln('pilihan tidak dikenal');
         end;
-    until piihan_menu =0;
+    until pilihan_menu = 0;
    // simpanFile();
 end.
